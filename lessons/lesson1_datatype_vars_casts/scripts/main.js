74ada09a21fcde708b9e8c078ccaf2f88889c51f
GrayHead <ss.zhuravlov@gmail.com>
2024-04-29 20:05:53 +0300

    lesson 1

---
+++ lessons/lesson1_datatype_vars_casts/scripts/main.js
@@ -0,0 +1,125 @@
+// 'use strict';
+//
+// var a = '43632';
+//
+// let b = 12332.321654798797;
+//
+//
+// const c = false;
+// // c = 'asudfgashgfd'; // error !!!!! this is constant
+// // d = 'asdasdfhagdsf';
+//
+// b = 100500;
+//
+// console.log(c);
+// console.log(b);
+//
+// let x = false;
+// console.log(typeof x);
+
+
+// let w1 = "okten";
+// let w2 = 'hello';
+//
+// let space = ' ';
+// let result = w1 + space + w2;
+// console.log(result);
+// console.log(100 + 200 + 'asdads');
+// let w3 = '!!!';
+// // result = w1 + space + w2 + space + w3;
+// result = `${w1} ${w2} ${w3}`;
+// console.log(result);
+
+
+// //NUMBERS
+//
+// let a = 100;
+// let b = 200;
+// console.log((a + b - 50) / 2);
+// // + - * /
+// console.log(11 % 2) //1
+// console.log(0.1 + 0.2);
+//
+// console.log(BigInt(1243154361436143261));
+
+
+// // let b = confirm();
+// let b = true;
+// console.log(b);
+// console.log(typeof b);
+// // > < >= <= == != === !==
+// console.log(5 > 6);
+// console.log(5 < 6);
+// console.log(5 >= 6);
+// console.log(6 <= 6);
+// console.log(6 == '6'); // true -> '6' == '6'
+// console.log(6 === 6);
+// console.log(6 !== 5); // true
+//
+//
+// console.log(!!'asdads');
+// console.log(!!-100500);
+//
+// console.log(!!0);//false
+// console.log(!!'');//false
+// console.log(!!false);//false
+// console.log(!!undefined);//false
+// console.log(!!null);//false
+// console.log(!!NaN);//false
+//
+//
+// console.log(typeof NaN);
+// console.log(!!NaN);
+
+
+// console.log(100 + ''); // string
+// console.log(+'100');
+// console.log(!!100);
+// console.log(!!'asd');
+// console.log(!!!'asd');
+// console.log(!'asd');
+
+
+let arr = [11, 22, 33, 44,true,{}];//object
+let user = {
+    ipn: 3216549874,
+    name: 'sergii',
+    surname: 'zhuravlov',
+    bday: '12-12-1987',
+    phone: '+380671231212'
+} // object
+
+
+
+
+console.log(arr);
+console.log(user);
+
+let users = [
+    {
+        id: 1,
+        name: 'asdads',
+        status: true
+    },
+    {
+        id: 2,
+        name: 'kokos',
+        status: false
+    },
+    {
+        id: 3,
+        name: 'abrikos',
+        status: true
+    }
+];
+
+
+
+
+
+
+
+
+
+
+
